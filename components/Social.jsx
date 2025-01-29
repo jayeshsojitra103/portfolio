abbb2615286a81ab9c7dd8b2602509d8d0cf9dc0
Jayesh Sojitra <jayesh.sojitra103@gmail.com>
2025-01-29 20:19:40 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { SiFiverr, SiUpwork } from "react-icons/si";
 
 const socials = [
   {
@@ -10,15 +9,7 @@ const socials = [
   {
     Icon: <FaLinkedin />,
     path: "https://www.linkedin.com/in/jayesh-sojitra-3031ba54/",
-  },
-  {
-    Icon: <SiFiverr />,
-    path: "https://www.fiverr.com/jayeshsojitra",
-  },
-  {
-    Icon: <SiUpwork />,
-    path: "https://www.upwork.com/freelancers/~018ed704edcaf0e007?mp_source=share",
-  },
+  }
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
